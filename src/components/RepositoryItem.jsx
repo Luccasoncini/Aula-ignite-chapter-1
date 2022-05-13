d6741eef3069cc36a1d1b0c6464e20381b6965fa
luccasoncini <luccadiassoncini@gmail.com>
2022-05-13 10:45:57 -0300

    usando useEffect e puxando de forma dinamica os dados da api do github

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,8 +2,8 @@ export function RepositoryItem({ repository }) {
     return (
         <li>
             <strong>{repository?.name ?? 'Default'}</strong>
-            <p>{repository?.description ?? 'Default'}</p>''
-            <a href={repository?.link ?? 'Default'}>Acessar repositório</a>
+            <p>{repository?.description ?? 'Default'}</p>
+            <a href={repository?.html_url ?? 'Default'}>Acessar repositório</a>
         </li>
     )
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,24 +1,30 @@
 import { RepositoryItem } from "./RepositoryItem";
+import { useEffect, useState } from "react";
 
+import '../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Form in React',
-    link: 'https://github.com/unform/unform'
-}
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository}/>
-                <br></br>
-                <RepositoryItem repository={repository}/>
-                <br></br>
-                <RepositoryItem repository={repository}/>
-                <br></br>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return (
+                        <RepositoryItem 
+                            repository={repository} 
+                        />
+                    )
+                })}
+                    
             </ul>
         </section>
     )
